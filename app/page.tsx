e0303a26b256848b5d5a2ddb645ac9c0a390e8d2
Ramit Pandey <ramitpandey2002@gmail.com>
2025-09-04 11:47:02 +0530

    setup: initialize Next.js project and add theme configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import { Button } from "../components/ui/button.tsx";
+
 export default function Home() {
-  return (
-    <>
-      <Button>Click me</Button>
-    </>
-  );
+  return <Button>Click me</Button>;
 }
