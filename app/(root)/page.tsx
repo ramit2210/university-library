9420196bc1ec63526f04332436d8f83faa919b9e
Ramit Pandey <ramitpandey2002@gmail.com>
2025-09-07 13:36:02 +0530

    feat: Refactor Book components and add BookCard and BookCover
    - Updated BookList to accept props for title and books, rendering BookCard for each book.
    - Enhanced BookOverview to display detailed book information and added a borrow button.
    - Introduced BookCard component for individual book representation with loan status.
    - Created BookCover component for displaying book cover images with customizable styles.
    - Added BookCoverSvg for SVG representation of book covers.
    - Updated next.config.ts to allow images from specific remote sources.
    - Added constants for navigation links, admin sidebar links, field names, and sample books.
    - Defined Book type in type.d.ts for better type safety across components.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,16 @@
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
+import { sampleBooks } from "@/constants";
 
 export default function Home() {
     return (
         <>
-            <BookOverview />
-            <BookList />
+            <BookOverview {...sampleBooks[0]} />
+            <BookList
+                title="Latest Books"
+                books={sampleBooks}
+                containerClassName="mt-28"
+            />
         </>
     );
 }
---
+++ type.d.ts
@@ -0,0 +1,15 @@
+type Book = {
+    id: number;
+    title: string;
+    author: string;
+    genre: string;
+    rating: number;
+    totalCopies: number;
+    availableCopies: number;
+    description: string;
+    color: string;
+    coverUrl: string;
+    vedio: string;
+    summary: string;
+    isLoanedBook?: boolean;
+};
